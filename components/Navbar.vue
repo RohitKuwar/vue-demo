8975211ffced6c0d54b42ee9a6c97c86a6d02df5
RohitKuwar <rohit1kuwar@gmail.com>
2021-04-18 10:28:19 +0530

    vue demo

---
+++ components/Navbar.vue
@@ -0,0 +1,80 @@
+<template>
+  <!-- <nav>
+    <a><nuxt-link to="/">Home</nuxt-link></a>
+    <a><nuxt-link to="/contact">Contact</nuxt-link></a>
+    <a><nuxt-link to="/about">About</nuxt-link></a>
+  </nav> -->
+  <div className="navbar">
+    <nav>
+      <ul>
+        <li>
+          <a href="https://realdevsquad.com/">
+            <img
+              src="~/assets/Real-Dev-Squad@1x.png"
+              alt="Real Dev Squad logo"
+            />
+          </a>
+        </li>
+        <li>
+          <a href="https://welcome.realdevsquad.com/">Welcome</a>
+        </li>
+        <li>
+          <a href="https://www.realdevsquad.com/events.html">Events</a>
+        </li>
+        <li>
+          <a href="https://members.realdevsquad.com/">Members</a>
+        </li>
+        <li>
+          <a href="https://crypto.realdevsquad.com/">Crypto</a>
+        </li>
+        <li>
+          <a href="https://status.realdevsquad.com/">Status</a>
+        </li>
+        <li>
+          <a href="https://goals.realdevsquad.com/">Goals</a>
+        </li>
+      </ul>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+}
+</script>
+
+<style scoped>
+nav {
+  background: #1d1283;
+}
+nav ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #1d1283;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+}
+nav li {
+  float: left;
+}
+nav li a {
+  display: block;
+  color: #ffffff;
+  text-align: center;
+  font-weight: bolder;
+  padding: 0 0.5em;
+  /* padding: 14px 16px; */
+  text-decoration: none;
+}
+nav li a:hover {
+  background-color: rgba(255, 255, 255, 0.2);
+}
+img {
+  width: 3.5em;
+  height: 3.5em;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <Navbar />
+    <h1>Welcome to Goals Site <span>&#127919;</span></h1>
+    <button>start your journey</button>
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Navbar from '../components/Navbar.vue'
+import Footer from '../components/Footer.vue'
+export default {
+  components: {
+    Navbar,
+    Footer,
+  },
+}
+</script>
+
+<style scoped>
+html,
+body {
+  font-family: 'Roboto', sans-serif;
+  width: 100%;
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+h1 {
+  color: black;
+  text-align: center;
+  font-size: 2.5em;
+  margin-top: 0.9em;
+  font-weight: 700;
+}
+button {
+  color: #ffffff;
+  background-color: #1d1283;
+  text-align: center;
+  font-size: 1.3em;
+  padding: 0.5em;
+  border: none;
+  outline: none;
+  border-radius: 0.2em;
+  text-transform: capitalize;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  cursor: pointer;
+}
+</style>
